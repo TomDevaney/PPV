82936c821867351316be389429f501495aa69689
Tom Devaney <Glorified Flashlight>
2016-12-01 08:59:01 -0500

    Starting to add Animation Design classes.

--- Ubihard_Project/Ubihard_Project/ResourceManager.h
+++ Ubihard_Project/Ubihard_Project/ResourceManager.h
@@ -1,9 +1,20 @@
 #pragma once
+#include "Animation.h"
+#include "AnimationSet.h"
+#include "BindPose.h"
+
+#include <map>
+
+//class Animation;
+//class BindPose;
+//class AnimationSet;
 
 class ResourceManager
 {
 private:
+	std::map<unsigned int, AnimationSet> animations;
+	std::map<unsigned int, BindPose*> rigs;
 
 public:
-
+	
 };
